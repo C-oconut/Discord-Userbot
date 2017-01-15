37b022a6b0af0cecc841ccfde28a86c936c4a67d
Sanjay <Sanjay-B@users.noreply.github.com>
2017-01-15 10:32:50 -0500

    Fixed LoopStart Login Error

--- startLoop.bat
+++ startLoop.bat
@@ -5,26 +5,23 @@ pushd %~dp0
 
 :loopstart
 
-::Attempts to start py launcher without relying on PATH
 %SYSTEMROOT%\py.exe --version > NUL 2>&1
 IF %ERRORLEVEL% NEQ 0 GOTO attempt
-%SYSTEMROOT%\py.exe -3.5 Bot.py
+%SYSTEMROOT%\py.exe -3.5 SanjayBot/Bot.py
 timeout 3
 GOTO loopstart
 
-::Attempts to start py launcher by relying on PATH
 :attempt
 py.exe --version > NUL 2>&1
 IF %ERRORLEVEL% NEQ 0 GOTO lastattempt
-py.exe -3.5 Bot.py
+py.exe -3.5 SanjayBot/Bot.py
 timeout 3
 GOTO loopstart
 
-::As a last resort, attempts to start whatever Python there is
 :lastattempt
 python.exe --version > NUL 2>&1
 IF %ERRORLEVEL% NEQ 0 GOTO message
-python.exe Bot.py
+python.exe SanjayBot/Bot.py
 timeout 3
 GOTO loopstart
 
